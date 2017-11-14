fee7ccc5091ef2c666686f34ed73df3724c22f80
gabrielmdias <gabriel_dias16@hotmail.com>
2017-11-14 15:29:19 -0200

    frist

---
+++ TrabPokemon/src/trabpokemon/Batalha.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabpokemon;
+
+/**
+ *
+ * @author gabriel
+ */
+public class Batalha {
+    
+    private final Jogador jogador1 = new Humano();
+    private final Jogador jogador2 = new Humano();
+    
+   
+    public void carregarTabelhas() {
+        
+    }
+    
+    public void inicializarJogadores() {
+        
+    }
+    
+    public void executarTurno() {
+        
+    }
+}
---
+++ TrabPokemon/src/trabpokemon/Jogador.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabpokemon;
+
+/**
+ *
+ * @author gabriel
+ */
+public abstract class Jogador {
+    
+    public abstract void escolherComando();
+    
+    public void trocarPokemon() {
+        
+    }
+    
+    public void usarAtaque() {
+        
+    }
+}
