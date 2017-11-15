6da237fe01e8455b93cd83fb00fc35871c68e61f
gabrielmdias <gabriel_dias16@hotmail.com>
2017-11-15 10:16:34 -0200

    inicializacao

--- TrabPokemon/src/trabpokemon/Batalha.java
+++ TrabPokemon/src/trabpokemon/Batalha.java
@@ -17,32 +17,33 @@ public class Batalha {
     private final Jogador[] maquina = new Maquina[2];
     
    
-    public void carregarTabelhas() {
+    public void carregarTabelas() {
         
     }
     
     public void inicializarJogadores() {
         Scanner scanner = new Scanner(System.in);
         int jogadorMaquina, qtdePokemon, especiePokemon, levelPokemon, atkPokemon[];
-        jogadorMaquina = scanner.nextInt();
-        qtdePokemon = scanner.nextInt();
-        System.out.println("Jogador: "+jogadorMaquina);
-        System.out.println("Quantidade: "+qtdePokemon);
-        for (int i = 0; i < qtdePokemon; i++) {
-            especiePokemon = scanner.nextInt();
-            levelPokemon = scanner.nextInt();
-            atkPokemon = new int[4];
-            for (int j = 0; j < 4; j++) {
-                atkPokemon[j] = scanner.nextInt();
+        for (int k = 0; k < 2; k++) {
+            jogadorMaquina = scanner.nextInt();
+            qtdePokemon = scanner.nextInt();
+            System.out.println("Jogador: "+jogadorMaquina);
+            System.out.println("Quantidade: "+qtdePokemon);
+            for (int i = 0; i < qtdePokemon; i++) {
+                especiePokemon = scanner.nextInt();
+                levelPokemon = scanner.nextInt();
+                atkPokemon = new int[4];
+                for (int j = 0; j < 4; j++) {
+                    atkPokemon[j] = scanner.nextInt();
+                }
+                System.out.println("Especie: "+ especiePokemon);
+                System.out.println("Level: "+ levelPokemon);
+                System.out.println("Atk: "+atkPokemon[0]);
+                System.out.println("Atk: "+atkPokemon[1]);
+                System.out.println("Atk: "+atkPokemon[2]);
+                System.out.println("Atk: "+atkPokemon[3]);
             }
-            System.out.println("Especie: "+ especiePokemon);
-            System.out.println("Level: "+ levelPokemon);
-            System.out.println("Atk: "+atkPokemon[0]);
-            System.out.println("Atk: "+atkPokemon[1]);
-            System.out.println("Atk: "+atkPokemon[2]);
-            System.out.println("Atk: "+atkPokemon[3]);
         }
-        
     }
     
     public void executarTurno() {
